699cbc6c092b872e2719bdde4062bbe24e98319e
runoob <test@runoob.com>
2019-06-04 14:14:26 +0800

    --nodejs 操作数据库

--- 8.连接mysql/app.js
+++ 8.连接mysql/app.js
@@ -1,3 +1,6 @@
 var db = require("./db");
-db.add("category node insert");
-db.showAll();
+//db.add("category node insert");
+//db.remove(18);
+//db.get(2);
+db.update("王五",8);
+//db.showAll();
--- 8.连接mysql/db.js
+++ 8.连接mysql/db.js
@@ -20,6 +20,7 @@ function showAll(){
 	connection.query(sql,function(err,results){
 		if(err){
 			console.log("[select error]-",err.message);
+			return;
 		}
 		if(results){
 			console.log("results:"+results);
@@ -39,13 +40,67 @@ function add(name){
 	connection.query(sql,params,function(err,result){
 		if(err){
 			console.log("[insert error]-",err.message);
+			return;
 		}
 		console.log("insert success, the generated id is:",result.insertId);
 	});
 	closeConnection();
 }
+//删除数据
+function remove(id){
+	console.log("进入删除方法");
+	openConnection();
+	var params=[id];
+	var sql=" delete from tbl_category where id =? ";
+	connection.query(sql,params,function(err,result){
+		if(err){
+			console.log("[remove error]-",err.message);
+			return;
+		}
+		console.log("remove id=%id success",id);
+	});
+	closeConnection();
+}
+//获取数据
+function get(id){
+	console.log("进入获取方法");
+	openConnection();
+	var params=[id];
+	var sql=" select * from tbl_category where id = ? ";
+	connection.query(sql,params,function(err,result){
+		if(err){
+			console.log("[remove error]-",err.message);
+			return;
+		}
+		if(result.length !=0){
+            var category = {id:result[0].id,name:result[0].name,sex:result[0].sex};
+            console.log("get category:"+ JSON.stringify(result))
+        }else{
+            console.log("not found with id :" + id)
+        }
+	});
+	closeConnection();
+}
+//更新数据
+function update(name,id){
+	console.log("进入更新方法");
+	openConnection();
+	var params=[name,id];
+	var sql=" update tbl_category set name = ? where id = ? ";
+	connection.query(sql,params,function(err,result){
+		if(err){
+			console.log("[update error]-",err.message);
+			return;
+		}
+		 console.log("update success"+result.affectedRows);
+	});
+	closeConnection();
+}
 exports.showAll = showAll;
 exports.add = add;
+exports.remove=remove;
+exports.get=get;
+exports.update=update;
 
 
 
